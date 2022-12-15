b91d2400549474fb4e369535a1e10107cb77a744
Anton0694 <91780319+Anton0694@users.noreply.github.com>
2022-12-15 14:51:36 +0200

    fix 2d hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,7 +10,7 @@ const player = new Player(iframe);
     player.on('timeupdate', throttle(onPlay, 1000))
     
 function onPlay({ seconds }) {
-    localStorage.setItem('videoplayer-current-time', seconds);
+    localStorage.setItem('setCurrentTime(|)', seconds);
 }
 
     
